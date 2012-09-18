66b6da3de388f83b4eab3501dd627118f48faab5
Christian Rasmussen <christian.rasmussen@me.com>
2012-09-18 19:29:51 +0200

    Initial Commit

---
+++ KulturkalenderTests/KulturkalenderTests.h
@@ -0,0 +1,13 @@
+//
+//  KulturkalenderTests.h
+//  KulturkalenderTests
+//
+//  Created by Christian Rasmussen on 18.09.12.
+//  Copyright (c) 2012 Under Dusken. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KulturkalenderTests : SenTestCase
+
+@end
---
+++ KulturkalenderTests/KulturkalenderTests.m
@@ -0,0 +1,32 @@
+//
+//  KulturkalenderTests.m
+//  KulturkalenderTests
+//
+//  Created by Christian Rasmussen on 18.09.12.
+//  Copyright (c) 2012 Under Dusken. All rights reserved.
+//
+
+#import "KulturkalenderTests.h"
+
+@implementation KulturkalenderTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in KulturkalenderTests");
+}
+
+@end
