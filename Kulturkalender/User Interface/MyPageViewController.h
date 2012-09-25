7ac135ed41b213e78e4f267f56aae7a78b789edc
Christian Rasmussen <christian.rasmussen@me.com>
2012-09-25 21:37:26 +0200

    Updated the storyboard and model.

---
+++ Kulturkalender/User Interface/MyPageViewController.h
@@ -0,0 +1,15 @@
+//
+//  MyPageViewController.h
+//  Kulturkalender
+//
+//  Created by Christian Rasmussen on 25.09.12.
+//  Copyright (c) 2012 Under Dusken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyPageViewController : UITableViewController
+
+- (IBAction)close:(UIStoryboardSegue *)segue;
+
+@end
