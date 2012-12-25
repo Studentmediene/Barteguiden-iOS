4a3accff8525a1129f265feabaed04e11c2b3fc7
Christian Rasmussen <christian.rasmussen@me.com>
2012-12-25 23:53:05 +0100

    Updated the settings to run test cases automatically after build. Added a new test case.

--- Kulturkalender/KulturkalenderTests/Business Logic/FilterManagerTests.m
+++ Kulturkalender/KulturkalenderTests/Business Logic/FilterManagerTests.m
@@ -26,7 +26,7 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet.");
+//    STFail(@"Unit tests are not implemented yet.");
 }
 
 @end
---
+++ Kulturkalender/KulturkalenderTests/User Interface/FilterViewControllerTests.m
@@ -0,0 +1,32 @@
+//
+//  FilterViewControllerTests.m
+//  Kulturkalender
+//
+//  Created by Christian Rasmussen on 25.12.12.
+//  Copyright (c) 2012 Under Dusken. All rights reserved.
+//
+
+#import "FilterViewControllerTests.h"
+
+@implementation FilterViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+//    STFail(@"Unit tests are not implemented yet.");
+}
+
+@end
