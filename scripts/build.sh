bc2f694ac4740fbcdaa58e8e79e286f175017cb0
Christian Rasmussen <christian.rasmussen@me.com>
2013-01-04 10:41:48 +0100

    Continuous integration did not work as planned.

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Halt on any errors
-set -e
-
-# Clean and build project
-xcodebuild -workspace default.xcworkspace -scheme Kulturkalender -configuration Release -sdk iphonesimulator clean build
--- scripts/run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Halt on any errors
-set -e
-
-# Run tests
-xcodebuild -workspace default.xcworkspace -scheme KulturkalenderTests -configuration Release -sdk iphonesimulator
